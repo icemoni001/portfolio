f1b049975969ce4191384e90690fcecf88c2e28a
icemoni001 <ebukamiracle220@gmail.com>
2024-09-20 15:35:11 +0100

    Add project files

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// Smooth scroll for navbar links
+document.querySelectorAll('a.nav-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+  
+  window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar');
+    
+    if (window.scrollY > 50) {  // Change '50' to adjust when the effect triggers
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
